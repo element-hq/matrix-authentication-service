15c02fa6431ee9210c225a679219b63ce914574e
Quentin Gliech <quenting@element.io>
2024-09-05 14:57:02 +0200

    Manual license edit of remaining files

--- crates/iana-codegen/src/main.rs
+++ crates/iana-codegen/src/main.rs
@@ -75,19 +75,11 @@ impl Display for File {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(
             f,
-            r#"// Copyright 2023 The Matrix.org Foundation C.I.C.
+            r#"// Copyright 2024 New Vector Ltd.
+// Copyright 2023, 2024 The Matrix.org Foundation C.I.C.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// SPDX-License-Identifier: AGPL-3.0-only
+// Please see LICENSE in the repository root for full details.
 
 #![allow(clippy::doc_markdown)]
 
