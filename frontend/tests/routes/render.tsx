5ec46be82c5967dc494fc1dec31fbb9e6f5520ca
Quentin Gliech <quenting@element.io>
2024-11-14 20:10:48 +0100

    Test the cross-signing reset flow

--- frontend/tests/routes/render.tsx
+++ frontend/tests/routes/render.tsx
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 // Please see LICENSE in the repository root for full details.
 
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createMemoryHistory } from "@tanstack/react-router";
 import { type RenderResult, render } from "@testing-library/react";
 import { TooltipProvider } from "@vector-im/compound-web";
@@ -11,9 +11,11 @@ import i18n from "i18next";
 import { setupServer } from "msw/node";
 import { I18nextProvider } from "react-i18next";
 import { afterAll, afterEach, beforeAll } from "vitest";
-import { queryClient } from "../../src/graphql";
-import { router } from "../../src/router";
 import { handlers } from "../mocks/handlers";
+import { createRouter } from "@tanstack/react-router";
+import { routeTree } from "../../src/routeTree.gen";
+
+// Create a new router instance
 
 export const server = setupServer(...handlers);
 
@@ -27,21 +29,21 @@ afterAll(() => server.close());
 afterEach(() => server.resetHandlers());
 
 async function renderPage(route: string): Promise<RenderResult> {
-  await router.load();
-
-  const history = createMemoryHistory({
-    initialEntries: [route],
+  // Create a new query client and a new router
+  const queryClient = new QueryClient();
+  const history = createMemoryHistory({ initialEntries: [route] });
+  const router = createRouter({
+    routeTree,
+    context: { queryClient },
+    history,
   });
+  await router.load();
 
   return render(
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <I18nextProvider i18n={i18n}>
-          <RouterProvider
-            router={router}
-            history={history}
-            context={{ queryClient }}
-          />
+          <RouterProvider router={router} />
         </I18nextProvider>
       </TooltipProvider>
     </QueryClientProvider>,
