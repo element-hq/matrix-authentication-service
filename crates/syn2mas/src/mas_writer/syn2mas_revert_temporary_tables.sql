0b13d6406c56563c75f23dbe2ddfb2d2228aa578
reivilibre <oliverw@element.io>
2025-01-02 18:21:39 +0000

    Foundation of new syn2mas tool (#3636)

---
+++ crates/syn2mas/src/mas_writer/syn2mas_revert_temporary_tables.sql
@@ -0,0 +1,12 @@
+-- Copyright 2024 New Vector Ltd.
+--
+-- SPDX-License-Identifier: AGPL-3.0-only
+-- Please see LICENSE in the repository root for full details.
+
+-- This script should revert what `syn2mas_temporary_tables.sql` does.
+
+DROP TABLE syn2mas_restore_constraints;
+DROP TABLE syn2mas_restore_indices;
+
+ALTER TABLE syn2mas__users RENAME TO users;
+ALTER TABLE syn2mas__user_passwords RENAME TO user_passwords;
