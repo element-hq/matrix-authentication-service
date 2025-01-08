99c88674ffbc0a6b3b95974b2961add8a30ff7e9
Quentin Gliech <quenting@element.io>
2025-01-08 15:24:26 +0100

    Better collapsible sections, and use them on the account page

---
+++ frontend/src/components/Collapsible/Collapsible.stories.tsx
@@ -0,0 +1,29 @@
+// Copyright 2025 New Vector Ltd.
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+// Please see LICENSE in the repository root for full details.
+
+import type { Meta, StoryObj } from "@storybook/react";
+import * as Collapsible from "./Collapsible";
+
+const meta = {
+  title: "UI/Collapsible",
+  component: Collapsible.Section,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Collapsible.Section>;
+
+export default meta;
+type Story = StoryObj<typeof Collapsible.Section>;
+
+export const Basic: Story = {
+  args: {
+    title: "Section name",
+    description: "Optional section description",
+    children: (
+      <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>Sed id felis eget orci aliquet tincidunt.</p>
+      </div>
+    ),
+  },
+};
