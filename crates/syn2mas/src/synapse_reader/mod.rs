dc91099c7a74952432fa38acb1e4144406037b2a
Olivier 'reivilibre <oliverw@matrix.org>
2025-02-02 17:59:37 +0000

    Remove TODO on MIGRATOR

--- crates/syn2mas/src/synapse_reader/mod.rs
+++ crates/syn2mas/src/synapse_reader/mod.rs
@@ -567,7 +567,6 @@ mod test {
         },
     };
 
-    // TODO test me
     static MIGRATOR: Migrator = sqlx::migrate!("./test_synapse_migrations");
 
     #[sqlx::test(migrator = "MIGRATOR", fixtures("user_alice"))]
