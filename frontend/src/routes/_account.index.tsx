589ff86529a1133cc4aac3fbe787e013c58f6ca4
Tonkku <contact@tonkku.me>
2025-03-10 15:35:19 +0000

    Frontend passkey management

--- frontend/src/routes/_account.index.tsx
+++ frontend/src/routes/_account.index.tsx
@@ -13,6 +13,7 @@ import {
 } from "@tanstack/react-router";
 import IconSignOut from "@vector-im/compound-design-tokens/assets/web/icons/sign-out";
 import { Button, Text } from "@vector-im/compound-web";
+import { Suspense } from "react";
 import { useTranslation } from "react-i18next";
 import * as v from "valibot";
 import AccountDeleteButton from "../components/AccountDeleteButton";
@@ -24,9 +25,11 @@ import LoadingSpinner from "../components/LoadingSpinner";
 import Separator from "../components/Separator";
 import { useEndBrowserSession } from "../components/Session/EndBrowserSessionButton";
 import AddEmailForm from "../components/UserProfile/AddEmailForm";
+import AddPasskeyForm from "../components/UserProfile/AddPasskeyForm";
 import UserEmailList, {
   query as userEmailListQuery,
 } from "../components/UserProfile/UserEmailList";
+import UserPasskeyList from "../components/UserProfile/UserPasskeyList";
 import { graphql } from "../gql";
 import { graphqlRequest } from "../graphql";
 
@@ -231,7 +234,10 @@ function Index(): React.ReactElement {
         {siteConfig.passkeysEnabled && (
           <>
             <Collapsible.Section title={t("frontend.account.passkeys.title")}>
-              placeholder text
+              <Suspense fallback={<LoadingSpinner />}>
+                <UserPasskeyList />
+                <AddPasskeyForm />
+              </Suspense>
             </Collapsible.Section>
 
             <Separator kind="section" />
