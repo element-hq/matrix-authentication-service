97d2b757c776585c90a60df283ae45eb023a2e3a
Quentin Gliech <quenting@element.io>
2025-03-13 13:27:32 +0100

    Add a comment on the migration stating that we keep an history of the policy data

--- crates/storage-pg/migrations/20250225091000_dynamic_policy_data.sql
+++ crates/storage-pg/migrations/20250225091000_dynamic_policy_data.sql
@@ -4,6 +4,10 @@
 -- Please see LICENSE in the repository root for full details.
 
 -- Add a table which stores the latest policy data
+--
+-- Every time the policy data is updated, it creates a new row, so that we keep
+-- an history of the policy data, trace back which version of the data was used
+-- on each evaluation.
 CREATE TABLE IF NOT EXISTS policy_data (
     policy_data_id UUID PRIMARY KEY,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
