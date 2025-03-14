d639e7def9edacfab79051512cf962cf6de9ac35
Tonkku <contact@tonkku.me>
2025-03-14 12:18:21 +0000

    Passkey login handler

--- crates/handlers/src/test_utils.rs
+++ crates/handlers/src/test_utils.rs
@@ -111,6 +111,7 @@ pub(crate) struct TestState {
     pub rng: Arc<Mutex<ChaChaRng>>,
     pub http_client: reqwest::Client,
     pub task_tracker: TaskTracker,
+    pub webauthn: Webauthn,
 
     #[allow(dead_code)] // It is used, as it will cancel the CancellationToken when dropped
     cancellation_drop_guard: Arc<DropGuard>,
@@ -253,6 +254,7 @@ impl TestState {
             rng,
             http_client,
             task_tracker,
+            webauthn,
             cancellation_drop_guard: Arc::new(shutdown_token.drop_guard()),
         })
     }
@@ -545,6 +547,12 @@ impl FromRef<TestState> for Limiter {
 impl FromRef<TestState> for reqwest::Client {
     fn from_ref(input: &TestState) -> Self {
         input.http_client.clone()
+    }
+}
+
+impl FromRef<TestState> for Webauthn {
+    fn from_ref(input: &TestState) -> Self {
+        input.webauthn.clone()
     }
 }
 
