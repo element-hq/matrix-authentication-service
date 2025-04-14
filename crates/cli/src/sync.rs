351c1e765485d5aa2ad642dce0d36bd31b223575
Adis Veletanlic <adis.veletanlic@gmail.com>
2025-04-14 18:52:41 +0200

    Format project

--- crates/cli/src/sync.rs
+++ crates/cli/src/sync.rs
@@ -191,7 +191,7 @@ pub async fn config_sync(
                     Some(encrypter.encrypt_to_string(client_secret.as_bytes())?)
                 } else if let Some(mut siwa) = provider.sign_in_with_apple.clone() {
                     // if private key file is defined and not private key (raw), we populate the private key
-                    // to hold the content of the private key file. private key (raw) takes precedence so 
+                    // to hold the content of the private key file. private key (raw) takes precedence so
                     // both can be defined without issues
                     if siwa.private_key.is_none() {
                         if let Some(private_key_file) = siwa.private_key_file.take() {
