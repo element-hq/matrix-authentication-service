2b1703eb2b63b70b2508c3d65d89a7ff7592c38c
Quentin Gliech <quenting@element.io>
2025-04-23 13:46:44 +0200

    Add a few missing license headers

--- crates/cli/src/commands/syn2mas.rs
+++ crates/cli/src/commands/syn2mas.rs
@@ -1,3 +1,8 @@
+// Copyright 2024, 2025 New Vector Ltd.
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+// Please see LICENSE in the repository root for full details.
+
 use std::{collections::HashMap, process::ExitCode, time::Duration};
 
 use anyhow::Context;
--- crates/syn2mas/src/mas_writer/checks.rs
+++ crates/syn2mas/src/mas_writer/checks.rs
@@ -1,4 +1,4 @@
-// Copyright 2024 New Vector Ltd.
+// Copyright 2024, 2025 New Vector Ltd.
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 // Please see LICENSE in the repository root for full details.
