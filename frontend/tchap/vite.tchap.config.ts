cb8a54cb5290ff0c253f812c2538c282029a010b
Olivier D <odelcroi@gmail.com>
2025-08-25 10:10:11 +0200

    add custom tchap and lasuite css  (#5)
    * add tchap configuration
    
    * add email mapping fallback rule
    
    * customize login page with proconnect button
    
    add comments
    
    move frontend config to tchap folder
    
    remove custom tchap
    
    replace cdt with tchap
    
    * revert login changes
    
    * update package-lock.json
    
    * add lasuite integration resources
    
    * fix lint
    
    ---------
    
    Co-authored-by: mcalinghee <mcalinghee.dev@gmail.com>

---
+++ frontend/tchap/vite.tchap.config.ts
@@ -0,0 +1,24 @@
+import { resolve } from "node:path";
+import { defineConfig, mergeConfig } from "vite";
+import viteConfig from "../vite.config";
+
+export default defineConfig((env) =>
+  mergeConfig(
+    viteConfig(env),
+    defineConfig({
+      //tchap config
+      build: {
+        rollupOptions: {
+          input: [
+            resolve(__dirname, "css/tchap.css"),
+            //resolve(__dirname, "../node_modules/@gouvfr-lasuite/integration/dist/css/prefixed-dsfr.css"),
+            resolve(
+              __dirname,
+              "../node_modules/@gouvfr-lasuite/integration/dist/css/homepage-full.css",
+            ),
+          ],
+        },
+      },
+    }),
+  ),
+);
