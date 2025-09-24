428d3042446e123558ff8b0873bee850513fd848
Olivier D <odelcroi@gmail.com>
2025-09-24 12:10:38 +0200

    Fix/ consent page  (#43)
    * make logo not clickable
    * redesign consent page

--- frontend/tchap/components/Footer/Footer.tsx
+++ frontend/tchap/components/Footer/Footer.tsx
@@ -30,14 +30,12 @@ const Footer: React.FC = () => (
   <footer className="lasuite fr-footer">
     <div className="fr-container lasuite-container">
       <div className="fr-footer__body">
-        <div className="fr-footer__brand fr-enlarge-link">
-          <a href="/" title="Retour à l'accueil - Tchap">
-            <p className="fr-logo">
-              République
-              <br />
-              Française
-            </p>
-          </a>
+        <div className="fr-footer__brand">
+          <p className="fr-logo">
+            République
+            <br />
+            Française
+          </p>
         </div>
       </div>
     </div>
