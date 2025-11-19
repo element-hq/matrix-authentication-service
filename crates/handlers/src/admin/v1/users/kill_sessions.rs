a928e184304a078a100a289d07ea2b39cc92be1c
mcalinghee <mcalinghee.dev@gmail.com>
2025-11-19 08:31:09 +0100

    add test

--- crates/handlers/src/admin/v1/users/kill_sessions.rs
+++ crates/handlers/src/admin/v1/users/kill_sessions.rs
@@ -14,8 +14,8 @@ use mas_storage::{
     queue::{QueueJobRepositoryExt as _, SyncDevicesJob},
     user::BrowserSessionFilter,
 };
+use tracing::error;
 use ulid::Ulid;
-use tracing::{error, info};
 
 use crate::{
     admin::{
@@ -35,6 +35,9 @@ pub enum RouteError {
 
     #[error("User ID {0} not found")]
     NotFound(Ulid),
+
+    #[error("User ID {0} has no session to kill")]
+    NoSession(Ulid),
 }
 
 impl_from_error_for_route!(mas_storage::RepositoryError);
@@ -45,6 +48,7 @@ impl IntoResponse for RouteError {
         let sentry_event_id = record_error!(self, Self::Internal(_));
         let status = match self {
             Self::Internal(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            Self::NoSession(_) => StatusCode::BAD_REQUEST,
             Self::NotFound(_) => StatusCode::NOT_FOUND,
         };
         (status, sentry_event_id, Json(error)).into_response()
@@ -89,34 +93,19 @@ pub async fn handler(
         .ok_or(RouteError::NotFound(id))?;
 
     let filter = CompatSessionFilter::new().for_user(&user).active_only();
-    let _affected = repo.compat_session().finish_bulk(&clock, filter).await?;
-
-    match _affected {
-        0 => info!("No active compatibility sessions to end"),
-        1 => info!("Ended 1 active compatibility session"),
-        _ => info!("Ended {_affected} active compatibility sessions"),
-    }
+    let compat_session_affected = repo.compat_session().finish_bulk(&clock, filter).await?;
 
     let filter = OAuth2SessionFilter::new().for_user(&user).active_only();
-    let _affected = repo.oauth2_session().finish_bulk(&clock, filter).await?;
-
-    match _affected {
-        0 => info!("No active compatibility sessions to end"),
-        1 => info!("Ended 1 active OAuth 2.0 session"),
-        _ => info!("Ended {_affected} active OAuth 2.0 sessions"),
-    }
+    let oauth2_session_affected = repo.oauth2_session().finish_bulk(&clock, filter).await?;
 
     let filter = BrowserSessionFilter::new().for_user(&user).active_only();
-    let _affected = repo.browser_session().finish_bulk(&clock, filter).await?;
+    let browser_session_affected = repo.browser_session().finish_bulk(&clock, filter).await?;
 
-    match _affected {
-        0 => info!("No active browser sessions to end"),
-        1 => info!("Ended 1 active browser session"),
-        _ => info!("Ended {_affected} active browser sessions"),
+    if compat_session_affected + oauth2_session_affected + browser_session_affected == 0 {
+        return Err(RouteError::NoSession(user.id));
     }
 
-    // // Schedule a job to sync the devices of the user with the homeserver
-    // warn!("Scheduling job to sync devices for the user");
+    // Schedule a job to sync the devices of the user with the homeserver
     repo.queue_job()
         .schedule_job(&mut rng, &clock, SyncDevicesJob::new(&user))
         .await?;
@@ -129,123 +118,125 @@ pub async fn handler(
     )))
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use chrono::Duration;
-//     use hyper::{Request, StatusCode};
-//     use mas_data_model::{Clock as _, Device};
-//     use sqlx::PgPool;
+#[cfg(test)]
+mod tests {
+    use chrono::Duration;
+    use hyper::{Request, StatusCode};
+    use mas_data_model::{Clock as _, Device};
+    use sqlx::PgPool;
 
-//     use crate::test_utils::{RequestBuilderExt, ResponseExt, TestState,
-// setup};
+    use crate::test_utils::{RequestBuilderExt, ResponseExt, TestState, setup};
 
-//     #[sqlx::test(migrator = "mas_storage_pg::MIGRATOR")]
-//     async fn test_finish_session(pool: PgPool) {
-//         setup();
-//         let mut state = TestState::from_pool(pool).await.unwrap();
-//         let token = state.token_with_scope("urn:mas:admin").await;
-//         let mut rng = state.rng();
+    #[sqlx::test(migrator = "mas_storage_pg::MIGRATOR")]
+    async fn test_kill_sessions(pool: PgPool) {
+        setup();
+        let mut state = TestState::from_pool(pool).await.unwrap();
+        let token = state.token_with_scope("urn:mas:admin").await;
+        let mut rng = state.rng();
 
-//         // Provision a user and a compat session
-//         let mut repo = state.repository().await.unwrap();
-//         let user = repo
-//             .user()
-//             .add(&mut rng, &state.clock, "alice".to_owned())
-//             .await
-//             .unwrap();
-//         let device = Device::generate(&mut rng);
-//         let session = repo
-//             .compat_session()
-//             .add(&mut rng, &state.clock, &user, device, None, false, None)
-//             .await
-//             .unwrap();
-//         repo.save().await.unwrap();
+        // Provision a user and a compat session
+        let mut repo = state.repository().await.unwrap();
+        let user = repo
+            .user()
+            .add(&mut rng, &state.clock, "alice".to_owned())
+            .await
+            .unwrap();
+        let device = Device::generate(&mut rng);
+        let session = repo
+            .compat_session()
+            .add(&mut rng, &state.clock, &user, device, None, false, None)
+            .await
+            .unwrap();
+        repo.save().await.unwrap();
 
-//         let request = Request::post(format!(
-//             "/api/admin/v1/misc/kill-sessions/{}/finish",
-//             session.id
-//         ))
-//         .bearer(&token)
-//         .empty();
-//         let response = state.request(request).await;
-//         response.assert_status(StatusCode::OK);
-//         let body: serde_json::Value = response.json();
+        let request = Request::post(format!("/api/admin/v1/users/{}/kill-sessions", &user.id))
+            .bearer(&token)
+            .empty();
+        let response = state.request(request).await;
+        response.assert_status(StatusCode::OK);
+        let body: serde_json::Value = response.json();
 
-//         // The finished_at timestamp should be the same as the current time
-//         assert_eq!(
-//             body["data"]["attributes"]["finished_at"],
-//             serde_json::json!(state.clock.now())
-//         );
-//     }
+        assert_eq!(body["data"]["id"], format!("{}", &user.id));
+        // The finished_at timestamp should be the same as the current time
+        let mut repo = state.repository().await.unwrap();
+        let expected = repo
+            .compat_session()
+            .lookup(session.id)
+            .await
+            .unwrap()
+            .unwrap();
+        assert_eq!(expected.finished_at().unwrap(), state.clock.now());
+    }
 
-//     #[sqlx::test(migrator = "mas_storage_pg::MIGRATOR")]
-//     async fn test_finish_already_finished_session(pool: PgPool) {
-//         setup();
-//         let mut state = TestState::from_pool(pool).await.unwrap();
-//         let token = state.token_with_scope("urn:mas:admin").await;
-//         let mut rng = state.rng();
+    #[sqlx::test(migrator = "mas_storage_pg::MIGRATOR")]
+    async fn test_kill_already_finished_session(pool: PgPool) {
+        setup();
+        let mut state = TestState::from_pool(pool).await.unwrap();
+        let token = state.token_with_scope("urn:mas:admin").await;
+        let mut rng = state.rng();
 
-//         // Provision a user and a compat session
-//         let mut repo = state.repository().await.unwrap();
-//         let user = repo
-//             .user()
-//             .add(&mut rng, &state.clock, "alice".to_owned())
-//             .await
-//             .unwrap();
-//         let device = Device::generate(&mut rng);
-//         let session = repo
-//             .compat_session()
-//             .add(&mut rng, &state.clock, &user, device, None, false, None)
-//             .await
-//             .unwrap();
+        // Provision a user and a compat session
+        let mut repo = state.repository().await.unwrap();
+        let user = repo
+            .user()
+            .add(&mut rng, &state.clock, "alice".to_owned())
+            .await
+            .unwrap();
+        let device = Device::generate(&mut rng);
+        let session = repo
+            .compat_session()
+            .add(&mut rng, &state.clock, &user, device, None, false, None)
+            .await
+            .unwrap();
 
-//         // Finish the session first
-//         let session = repo
-//             .compat_session()
-//             .finish(&state.clock, session)
-//             .await
-//             .unwrap();
+        // Finish the session first
+        let session = repo
+            .compat_session()
+            .finish(&state.clock, session)
+            .await
+            .unwrap();
 
-//         repo.save().await.unwrap();
+        repo.save().await.unwrap();
 
-//         // Move the clock forward
-//         state.clock.advance(Duration::try_minutes(1).unwrap());
+        // Move the clock forward
+        state.clock.advance(Duration::try_minutes(1).unwrap());
 
-//         let request = Request::post(format!(
-//             "/api/admin/v1/misc/kill-sessions/{}/finish",
-//             session.id
-//         ))
-//         .bearer(&token)
-//         .empty();
-//         let response = state.request(request).await;
-//         response.assert_status(StatusCode::BAD_REQUEST);
-//         let body: serde_json::Value = response.json();
-//         assert_eq!(
-//             body["errors"][0]["title"],
-//             format!(
-//                 "Compatibility session with ID {} is already finished",
-//                 session.id
-//             )
-//         );
-//     }
+        let request = Request::post(format!("/api/admin/v1/users/{}/kill-sessions", &user.id))
+            .bearer(&token)
+            .empty();
+        let response = state.request(request).await;
+        response.assert_status(StatusCode::BAD_REQUEST);
+        let body: serde_json::Value = response.json();
 
-//     #[sqlx::test(migrator = "mas_storage_pg::MIGRATOR")]
-//     async fn test_finish_unknown_session(pool: PgPool) {
-//         setup();
-//         let mut state = TestState::from_pool(pool).await.unwrap();
-//         let token = state.token_with_scope("urn:mas:admin").await;
+        assert_eq!(
+            body["errors"][0]["title"],
+            format!("User ID {} has no session to kill", &user.id)
+        );
+        let mut repo = state.repository().await.unwrap();
+        let expected = repo
+            .compat_session()
+            .lookup(session.id)
+            .await
+            .unwrap()
+            .unwrap();
+        assert_ne!(expected.finished_at().unwrap(), state.clock.now());
+    }
 
-//         let request =
-//
-// Request::post("/api/admin/v1/misc/kill-sessions/01040G2081040G2081040G2081/
-// finish")                 .bearer(&token)
-//                 .empty();
-//         let response = state.request(request).await;
-//         response.assert_status(StatusCode::NOT_FOUND);
-//         let body: serde_json::Value = response.json();
-//         assert_eq!(
-//             body["errors"][0]["title"],
-//             "Compatibility session with ID 01040G2081040G2081040G2081 not
-// found"         );
-//     }
-// }
+    #[sqlx::test(migrator = "mas_storage_pg::MIGRATOR")]
+    async fn test_kill_sessions_on_unknown_users(pool: PgPool) {
+        setup();
+        let mut state = TestState::from_pool(pool).await.unwrap();
+        let token = state.token_with_scope("urn:mas:admin").await;
+
+        let request = Request::post("/api/admin/v1/users/01040G2081040G2081040G2081/kill-sessions")
+            .bearer(&token)
+            .empty();
+        let response = state.request(request).await;
+        response.assert_status(StatusCode::NOT_FOUND);
+        //         let body: serde_json::Value = response.json();
+        //         assert_eq!(
+        //             body["errors"][0]["title"],
+        //             "Compatibility session with ID 01040G2081040G2081040G2081
+        // not found"         );
+    }
+}
