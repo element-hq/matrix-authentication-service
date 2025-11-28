be900cfb78db485220d214d8855a2249f268a99b
Ben Banfield-Zanin <benbz@element.io>
2025-11-28 15:18:53 +0000

    Add 'IF NOT EXISTS' to all 'CREATE INDEX CONCURRENTLY' statements to avoid deadlocks

--- crates/storage-pg/migrations/20250410000035_idx_user_recovery_tickets_session_fk.sql
+++ crates/storage-pg/migrations/20250410000035_idx_user_recovery_tickets_session_fk.sql
@@ -4,6 +4,6 @@
 -- SPDX-License-Identifier: AGPL-3.0-only
 -- Please see LICENSE in the repository root for full details.
 
-CREATE INDEX CONCURRENTLY
+CREATE INDEX CONCURRENTLY IF NOT EXISTS
   user_recovery_tickets_session_fk
   ON user_recovery_tickets (user_recovery_session_id);
--- crates/storage-pg/migrations/20250410000044_idx_users_primary_email_fk.sql
+++ crates/storage-pg/migrations/20250410000044_idx_users_primary_email_fk.sql
@@ -6,6 +6,6 @@
 
 -- We don't use this column anymore, but… it will still tank the performance on
 -- deletions of user_emails if we don't have it
-CREATE INDEX CONCURRENTLY
+CREATE INDEX CONCURRENTLY IF NOT EXISTS
   users_primary_email_fk
   ON users (primary_user_email_id);
